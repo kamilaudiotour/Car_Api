17f5162d472e7dc791ad653063aac7d8d687aa0c
Kacper Gryka <phillipekcoutinho@gmail.com>
2022-10-10 21:21:19 +0200

    delete unnecessary classes

--- app/src/main/java/com/example/carapi/ui/CarViewModel.kt
+++ app/src/main/java/com/example/carapi/ui/CarViewModel.kt
@@ -3,11 +3,10 @@ package com.example.carapi.ui
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import com.example.carapi.util.Resource
 import com.example.carapi.models.Car
 import com.example.carapi.repository.CarRepository
+import com.example.carapi.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
@@ -33,8 +32,8 @@ class CarViewModel @Inject constructor(
 
     }
 
-    private fun handleCarResponse(response: Response<List<Car>>) : Resource<List<Car>> {
-        if(response.isSuccessful){
+    private fun handleCarResponse(response: Response<List<Car>>): Resource<List<Car>> {
+        if (response.isSuccessful) {
             response.body()?.let { resultResponse ->
                 Log.d("response", response.body().toString())
                 return Resource.Success(resultResponse)

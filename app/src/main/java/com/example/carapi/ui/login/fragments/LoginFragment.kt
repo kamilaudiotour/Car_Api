acc8527b749b476a6a6c26bc13a942e67fde02b5
Kacper Gryka <phillipekcoutinho@gmail.com>
2022-11-24 21:04:49 +0100

    some new animations

--- app/src/main/java/com/example/carapi/ui/login/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/carapi/ui/login/fragments/LoginFragment.kt
@@ -38,6 +38,10 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                 val password = passwordEt.text.toString()
                 viewModel.login(email, password)
             }
+            newAccountTv.setOnClickListener {
+                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+
         }
 
         viewModel.login.observe(viewLifecycleOwner) {
